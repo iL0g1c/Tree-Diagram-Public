83d2273389a18cd6b3d41622d00bfc66177d6d4d
Matthew Todd <mathtodd05@gmail.com>
2026-03-18 15:24:47 +0000

    Configured for deployment.

---
+++ src/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: "TreeDiagram",
+        script: "treeDiagramPublic.py",
+        interpreter: "../.venv/bin/python",
+        env: {
+
+        },
+      },
+    ],
+  };
